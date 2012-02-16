7cd0a95ffcf84ec0faf41c1c36cafec0be9aeceb
Benjamin Geiger <benjamingeiger@siwoti.com>
2012-02-16 14:50:14 -0500

    Parse the connections file.

--- FileParsers.py
+++ FileParsers.py
@@ -40,9 +40,44 @@ def parse_locations_file(locationsfile):
     return locations
 
         
+def parse_connections_file(connectionsfile, locations):
+
+    for line in connectionsfile.readlines():
+        # Cut off anything after a hash, then split the rest on whitespace.
+        tokens = line.split("#")[0].split()
+
+        if len(tokens) == 0:
+            continue
+
+        if len(tokens) == 1 and tokens[0].lower() == "end":
+            return locations
+
+        try:
+            currentlocation = [x for x in locations if x.name == tokens[0]][0]
+        except ex as IndexError:
+            print("Invalid connection (could not find source city):", line)
+            continue
+
+        for neighborname in tokens[2:]:
+            try:
+                neighbor = [x for x in locations if x.name == neighborname][0]
+            except ex as IndexError:
+                print("Invalid connection (could not find destination " + neighborname + ")", line)
+                continue
+
+            currentlocation.neighbors.append(neighbor)
+
+    return locations
 
 
 if __name__ == "__main__":
-    print(parse_locations_file(open("locations.txt")))
+    locations = parse_locations_file(open("locations.txt"))
+    connectedlocations = parse_connections_file(open("connections.txt"), locations)
+    for l in connectedlocations:
+        print(l.name, end=" ")
+        print(len(l.neighbors), end=": ")
+        for n in l.neighbors:
+            print(n.name, end=" ")
+        print()
 
 # vim: set et sw=4 ts=4: 
