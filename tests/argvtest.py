67a8a00a480fbc2a665ff585b0f1e970a6897915
Benjamin Geiger <benjamingeiger@siwoti.com>
2012-02-23 15:44:39 -0500

    Add misc test file.

---
+++ tests/argvtest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# 
+#
+# Description.
+#
+# Copyright (c) 2012 Benjamin Geiger <begeiger@mail.usf.edu>
+
+
+
+import sys
+
+for arg in sys.argv:
+    print(arg)
+
+
+# vim: set et sw=4 ts=4: 
