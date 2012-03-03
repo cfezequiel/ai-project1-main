ab4f31239c403f22414f155677f8d2bcdcf66dd8
Benjamin Geiger <benjamingeiger@siwoti.com>
2012-03-02 22:15:47 -0500

    Add run_to_end(), a convenience function.

--- AStarSearch.py
+++ AStarSearch.py
@@ -15,17 +15,39 @@ class AStarSearch (object):
     (Retrieved 16 February 2012)"""
 
     def __init__ (self, origin, destination, heuristic, distance_func = None, ghetto=[]):
+
         self.origin = origin
         self.destination = destination
-        self.ghetto = ghetto
+        self.ghetto = ghetto # TODO: find a non-racist name for this.
 
         self.heuristic = heuristic
         self.distance_func = distance_func or lambda x, y: x.distance_to(y)
 
-        self.openset = [(origin, 0, heuristic(origin), None)]
-        self.closedset = []
+        self._frontier = [(origin, 0, heuristic(origin), None)]
+        self._explored = []
 
     def run_to_end (self):
+        """Convenience method to find the shortest path.
+        
+        Also serves as a programming-by-wishful-thinking setup, so I know
+        which functions I still need to write."""
+
+        while self.frontier_size() > 0:
+            # Should we make these variables class-level? Probably.
+            current_node, distance, estimate = self.get_next_node()
+            if current_node == None:
+                raise Exception # This really shouldn't happen.
+            if current_node == destination:
+                return self.generate_path_from(current_node)
+
+            for neighbor in self.get_neighbors(current_node):
+                n_distance = self.distance_func(current_node, neighbor)
+
+                self.update_frontier(current_node, neighbor, distance)
+
+        raise Exception # No route from start to end.
+
+
 
 
         
