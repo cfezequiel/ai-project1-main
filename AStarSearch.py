84b4ee8b0a1e77cbee40169e23b39fe66481c924
Benjamin Geiger <benjamingeiger@siwoti.com>
2012-03-02 22:29:28 -0500

    Finish implementing methods for the search.

--- AStarSearch.py
+++ AStarSearch.py
@@ -47,10 +47,49 @@ class AStarSearch (object):
 
         raise Exception # No route from start to end.
 
+    def get_next_node (self):
+        current_item = min(self.frontier, key=lambda x:x[1] + x[2])
 
+        self.frontier.remove(current_item)
+        self.explored.append(current_item)
 
+        return (current_item[0], current_item[1], current_item[2])
 
+    def generate_path_from (self, node):
+        path = []
+        parent = node
+        while parent is not None:
+            path.append(parent)
+            try:
+                parent = [x[3] for x in self.explored if x[0] == parent][0]
+            except ex as IndexError:
+                parent = None
+        path.reverse()
         
+        return path
+
+    def get_neighbors (self, node):
+        """Get all neighbors that haven't yet been explored."""
+
+        # Yes, it's a one-line method. It's just a really ugly line.
+        return [n for n in node.neighbors if n not in [x[0] for x in self.explored] if heuristic(n) is not None]
+
+    def update_frontier (self, current_node, new_node, current_distance):
+        n_distance = self.distance_func(current_node, new_node)
+        if neighbor not in [x[0] for x in self.frontier]:
+            self.frontier.append((neighbor,
+                    current_distance + n_distance,
+                    heuristic(new_node),
+                    current_node))
+        else:
+            old_route = [x for x in self.frontier if x[0] == neighbor][0]
+
+            if current_distance + n_distance < old_route[1]:
+                self.frontier.remove(old_route)
+                self.frontier.append((neighbor,
+                        current_distance + n_distance,
+                        heuristic(new_node),
+                        current_node))
 
 if __name__ == "__main__":
 	print("Insert unit test here.")
