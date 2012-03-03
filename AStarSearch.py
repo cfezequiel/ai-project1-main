9a7e7d26d4dd71f171b51243a340910250714cc9
Benjamin Geiger <benjamingeiger@siwoti.com>
2012-03-02 22:40:15 -0500

    Make the search object handle avoiding ghettos.
    Also, I really need to find a better (that is,
    less racist) name for "ghettos".

--- AStarSearch.py
+++ AStarSearch.py
@@ -72,7 +72,7 @@ class AStarSearch (object):
         """Get all neighbors that haven't yet been explored."""
 
         # Yes, it's a one-line method. It's just a really ugly line.
-        return [n for n in node.neighbors if n not in [x[0] for x in self._explored] if self.heuristic(n) is not None]
+        return [n for n in node.neighbors if n not in [x[0] for x in self._explored] if n not in self.ghetto]
 
     def frontier_size (self):
         return len(self._frontier)
