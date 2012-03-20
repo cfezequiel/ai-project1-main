85228b6525b5dd98a8339f39f5918000b0f9f2ae
Benjamin Geiger <begeiger@mail.usf.edu>
2012-03-20 14:01:27 -0400

    Modify the search to work with roads instead of cities.

--- AStarSearch.py
+++ AStarSearch.py
@@ -6,6 +6,8 @@
 #
 # Copyright (c) 2012 Benjamin Geiger <begeiger@mail.usf.edu>
 
+from Road import Road
+from City import City
 
 class AStarSearch (object):
     """Represents an A* search on a graph.
@@ -36,7 +38,7 @@ class AStarSearch (object):
         self.distance_func = distance_func or (lambda x, y: x.distance_to(y))
 
     def start_search (self):
-        self._frontier = [(self.origin, 0, self.heuristic(self.origin), None)]
+        self._frontier = [(Road(self.origin, self.origin), 0, self.heuristic(self.origin))]
         self._explored = []
 
     def run_to_end (self):
@@ -47,26 +49,28 @@ class AStarSearch (object):
 
         while self.frontier_size() > 0:
             # Should we make these variables class-level? Probably.
-            current_node, distance, estimate = self.get_next_node()
-            if current_node == None:
+            current_road, distance, estimate = self.get_next_road()
+            if current_road == None:
                 raise Exception # This really shouldn't happen.
+            current_node = current_road.destination
             if current_node == self.destination:
                 return self.generate_path_from(current_node)
 
-            for neighbor in self.get_neighbors(current_node):
+            for neighbor_road in self.get_neighbors(current_node):
+                neighbor = neighbor_road.destination
                 n_distance = self.distance_func(current_node, neighbor)
 
-                self.update_frontier(current_node, neighbor, distance)
+                self.update_frontier(neighbor_road, distance)
 
         raise Exception # No route from start to end.
 
-    def get_next_node (self):
+    def get_next_road (self):
         current_item = min(self._frontier, key=lambda x:x[1] + x[2])
 
         self._frontier.remove(current_item)
         self._explored.append(current_item)
 
-        return (current_item[0], current_item[1], current_item[2])
+        return current_item
 
     def generate_path_from (self, node):
         path = []
@@ -74,9 +78,14 @@ class AStarSearch (object):
         while parent is not None:
             path.append(parent)
             try:
-                parent = [x[3] for x in self._explored if x[0] == parent][0]
-            except ex as IndexError:
+                nextparent = [x[0].origin for x in self._explored if x[0].destination == parent][0]
+            except IndexError as ex:
+                nextparent = None
+
+            if nextparent == parent:
                 parent = None
+            else:
+                parent = nextparent
         path.reverse()
         
         return path
@@ -85,30 +94,28 @@ class AStarSearch (object):
         """Get all neighbors that haven't yet been explored."""
 
         # Yes, it's a one-line method. It's just a really ugly line.
-        return [n for n in [x.destination for x in node.neighbors] if n not in [x[0] for x in self._explored] if n not in self.potholes]
+        return [n for n in node.neighbors if n.destination not in [x[0] for x in self._explored] if n.destination not in self.potholes]
 
     def frontier_size (self):
         return len(self._frontier)
 
-    def update_frontier (self, current_node, new_node, current_distance):
-        n_distance = self.distance_func(current_node, new_node)
-        if new_node not in [x[0] for x in self._frontier]:
-            self._frontier.append((new_node,
+    def update_frontier (self, road, current_distance):
+        n_distance = self.distance_func(road.origin, road.destination)
+        if road.destination not in [x[0].destination for x in self._frontier]:
+            self._frontier.append((road,
                     current_distance + n_distance,
-                    self.heuristic(new_node),
-                    current_node))
+                    self.heuristic(road.destination)))
         else:
-            old_route = [x for x in self._frontier if x[0] == new_node][0]
+            old_route = [x for x in self._frontier if x[0].destination == road.destination][0]
 
             if current_distance + n_distance < old_route[1]:
                 self._frontier.remove(old_route)
-                self._frontier.append((new_node,
+                self._frontier.append((road,
                         current_distance + n_distance,
-                        self.heuristic(new_node),
-                        current_node))
+                        self.heuristic(road.destination)))
 
     def get_frontier_city_names (self):
-        return [c[0].name for c in self._frontier]
+        return [c[0].destination.name for c in self._frontier]
 
 if __name__ == "__main__":
 	print("Insert unit test here.")
